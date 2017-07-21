c1c21e058d0412a3b0213c676ddbd066f011ba24
Haunter17 <haunterhacker17@gmail.com>
2017-07-20 17:20:13 -0700

    fixing column index

--- align_eval/dtw_batch.m
+++ align_eval/dtw_batch.m
@@ -1,39 +1,30 @@
-function [ output_args ] = dtw_batch( wav_list, annot_list, signal_x, annot_x, outdir )
+function [ output_args ] = dtw_batch(filelist, signal_midi, annot_midi, outdir )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 
-fid = fopen(wav_list);
-wavFileList = '';
+fid = fopen(filelist);
+fileNameList = '';
 fileIndex = 1;
 curfile = fgetl(fid);
 while ischar(curfile)
-    wavFileList{fileIndex} = curfile;
+    fileNameList{fileIndex} = curfile;
     curfile = fgetl(fid);
     fileIndex = fileIndex + 1;
 end
 fclose(fid);
 
-fid = fopen(annot_list);
-annotFileList = '';
-fileIndex = 1;
-curfile = fgetl(fid);
-while ischar(curfile)
-    annotFileList{fileIndex} = curfile;
-    curfile = fgetl(fid);
-    fileIndex = fileIndex + 1;
-end
-fclose(fid);
-
-parfor index = 1:length(wavFileList)
-    wavfile = wavFileList{index};
-    [pathstr,name,ext] = fileparts(wavfile);
-    disp(['Generating alignment on ',name]);
-    signal_y = audioread(wavfile);
-    annotfile = annotFileList{index};
-    annot_y = csvread(annotfile); % to fix later
+parfor index = 1:length(fileNameList)
+    fileName = fileNameList{index};
+    [pathstr,name,ext] = fileparts(fileName);
+    disp(['==> Generating alignment on ',name]);
+    wavfile = strcat(fileName, '.wav');
+    signal_perf = audioread(wavfile);
+    annotfile = strcat(fileName, '.csv');
+    annot_perf = csvread(annotfile);
+    annot_perf = annot_perf(:, 1);
     savefile = strcat(outdir, name);
-    [dist, ix, iy] = dtw_single(signal_x, signal_y, annot_x, ...
-            annot_y, savefile, 22050);
+    [dist, ix, iy] = dtw_single(signal_midi, signal_perf, annot_midi, ...
+            annot_perf, savefile, 500, 22050);
 end
 
 end
--- align_eval/dtw_single.m
+++ align_eval/dtw_single.m
@@ -1,24 +1,30 @@
-function [dist, ix, iy] = dtw_single(signal_x, signal_y, annot_x, annot_y, savefile, fs)
+function [dist, ix, iy] = dtw_single(signal_x, signal_y, annot_x, annot_y,...
+    savefile, downsampling, fs)
 % arguments:
 	%			signal_x: signal x to be aligned
   	%			signal_y: signal y to be aligned  
     %           annot_x: annotation time stamps for signal x (in sec)
     %           annot_y: annotation time stamps for signal y (in sec)
     %           savefile: path to save the plots
+    %           downsampling: the rate of downsampling
 	%			fs: optional param, the sampling rate
 if nargin < 6
     fs = 22050;
 end
+fs = round(fs / downsampling);
+signal_x = signal_x(1 : downsampling : end);
+signal_y = signal_y(1 : downsampling : end);
 
 [dist, ix, iy] = dtw(signal_x, signal_y);
-plot(ix, iy, '-.');
+plot(ix, iy, '-o', 'DisplayName','alignment');
 ylim([1, length(iy)]);
 hold on;
 ind_annot_x = fs * annot_x;
 ind_annot_y = fs * annot_y;
-plot(ind_annot_x, ind_annot_y, 'D');
+plot(ind_annot_x, ind_annot_y, '*', 'DisplayName','groundtruth');
 hold off;
-print('savefile','-dpng');
+legend('show')
+print(savefile,'-dpng');
 
 end
 
